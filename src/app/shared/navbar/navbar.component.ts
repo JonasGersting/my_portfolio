9a7af839c94c8fadb5688ade946564bcddc00cb2
Jonas Gersting <jonasgersting@web.de>
2025-01-23 00:55:21 +0100

    navbar finished and hero section  started

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,12 +1,20 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [ CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  selectedLang: string = 'DE'; 
+  
+
+
+  setLanguage(lang: string): void {
+    this.selectedLang = lang;
+  }
 
 }
