d5532cc764e7841889f8727e5ee9eb95b8cfb359
Jonas Gersting <jonasgersting@web.de>
2025-01-26 14:27:36 +0100

    Why me section added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
+import { WhyMeComponent } from './why-me/why-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, WhyMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
