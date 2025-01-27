f09edf1e0bbac650eebcf97aaf25a8dca9f81d56
Jonas Gersting <jonasgersting@web.de>
2025-01-28 00:09:59 +0100

    my projects finished

--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -14,6 +14,17 @@ export class MyProjectsComponent {
   projects: any = [
     { title: 'El pollo Loco',
       headline1:'About the project',
+      description1: 'This App is a Slack Clone App. <br> It revolutionizes team communication and collaboratin with its intuitive <br> interface, real-time messaging, and robust channel organization.', 
+      duration: '3 Weeks',
+      headline2: 'How I have organised my work', 
+      description2: 'This App is a Slack Clone App. <br> It revolutionizes team communication and collaboratin with its intuitive <br> interface, real-time messaging, and robust channel organization.',
+      headline3: ' My group work experience',
+      description3: 'This App is a Slack Clone App. <br> It revolutionizes team communication and collaboratin with its intuitive <br> interface, real-time messaging, and robust channel organization.',
+      technologies: ['./../../assets/img/HTML.png', './../../assets/img/CSS.png', './../../assets/img/Javascript.png'],
+      img: './../../assets/img/screenshotProject.png'
+    },
+    { title: 'Join',
+      headline1:'Join',
       description1: 'A spicy chicken restaurant app', 
       duration: '3 Weeks',
       headline2: 'How I have organised my work', 
@@ -23,8 +34,8 @@ export class MyProjectsComponent {
       technologies: ['./../../assets/img/HTML.png', './../../assets/img/CSS.png', './../../assets/img/Javascript.png'],
       img: './../../assets/img/screenshotProject.png'
     },
-    { title: 'Join',
-      headline1:'Join',
+    { title: 'Ring of Fire',
+      headline1:'Ring of Fire',
       description1: 'A spicy chicken restaurant app', 
       duration: '3 Weeks',
       headline2: 'How I have organised my work', 
