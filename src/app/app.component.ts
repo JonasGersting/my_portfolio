e6fbda28b9fbd522d1a5d9a1c2f103adc9aa8ea9
Jonas Gersting <jonasgersting@web.de>
2025-01-27 17:48:54 +0100

    projects component started styling and implementing project logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,12 @@ import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { HeroComponent } from "./hero/hero.component";
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
+import { MyProjectsComponent } from "./my-projects/my-projects.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, WhyMeComponent, MySkillsComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/my-projects/my-projects.component.ts
+++ src/app/my-projects/my-projects.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ProjectComponent } from "./project/project.component";
 
 @Component({
   selector: 'app-my-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ProjectComponent, ProjectComponent],
   templateUrl: './my-projects.component.html',
-  styleUrl: './my-projects.component.scss'
+  styleUrls: ['./my-projects.component.scss']
 })
 export class MyProjectsComponent {
-
+  activeButton: number | null = 1;  
+  activateBtn(position: number) {
+    if (this.activeButton === position) {
+      this.activeButton = null;
+    } else {
+      this.activeButton = position;
+    }
+  }
 }
