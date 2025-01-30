73ad302ba9b13e0c0079d69f961c43e4bacdddc3
Jonas Gersting <jonasgersting@web.de>
2025-01-30 01:20:56 +0100

    teamplayerSection added and appear animation for each component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
@@ -6,14 +6,39 @@ import { HeroComponent } from "./hero/hero.component";
 import { WhyMeComponent } from './why-me/why-me.component';
 import { MySkillsComponent } from './my-skills/my-skills.component';
 import { MyProjectsComponent } from "./my-projects/my-projects.component";
+import { TeamplayerComponent } from './teamplayer/teamplayer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+  imports: [CommonModule, RouterOutlet, NavbarComponent, HeroComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, TeamplayerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'portfolio';
+
+  ngAfterViewInit(): void {
+    this.addIntersectionObserver();
+  }
+
+  private addIntersectionObserver() {
+    const observerOptions = {
+      root: null,
+      threshold: 0.2
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('fade-in-right');
+        }
+      });
+    }, observerOptions);
+
+
+    const targets = document.querySelectorAll('.scroll-animate');
+    targets.forEach(target => observer.observe(target));
+  }
+
 }
---
+++ src/app/teamplayer/teamplayer.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teamplayer',
+  standalone: true,
+  imports: [],
+  templateUrl: './teamplayer.component.html',
+  styleUrl: './teamplayer.component.scss'
+})
+export class TeamplayerComponent {
+
+  presentation: any = [
+    {
+      title: 'Teamwork',
+      text: 'I excel in team environments, as demonstrated during a Kanban project named "Join," where I collaborated closely with two colleagues. Effective communication and coordination were key to our success.',
+      img:'./../../../assets/img/group.png'
+    },
+    {
+      title: 'Independent',
+      text: 'I thrive working independently,    proven by my development of a document visualization tool at work. This self-initiated project is now actively used, showcasing my ability to take initiative and deliver.',
+      img:'./../../../assets/img/rocket.png'
+    },
+    {
+      title: 'Adaptable',
+      text: 'I adapt seamlessly to any situation, combining teamwork and independence. Whether collaborating or working solo, I adjust to meet the needs of the project efficiently.',
+      img:'./../../../assets/img/scale.png'
+    }
+  ]
+}
