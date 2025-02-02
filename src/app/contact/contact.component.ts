9bbf4941e7d9adaf8256c18dfecb4723f80251a1
Jonas Gersting <jonasgersting@web.de>
2025-02-02 16:38:05 +0100

    finished the contact fornular. added validation and invalid markings

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,17 +1,34 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss'
+  styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
   isPrivacyPolicyChecked = false;
+  yourName = '';
+  yourMail = '';
+  yourMessage = '';
 
-  submit(){
-   
+  labelName = 'Your Name';
+  labelNameError = 'Please enter your Name';
+  labelMail = 'Your Email';
+  labelMailError = 'Please enter a valid email address';
+  labelMessage = 'Your Message';
+  labelMessageError = 'Please enter a message';
+  submit(form: NgForm) {
+    if (form.invalid) {
+      Object.values(form.controls).forEach(control => control.markAsTouched());
+      console.error('Formular ungültig!');
+      return;
+    }
+  
+    console.log('Formular erfolgreich gesendet:', form.value);
   }
+  
 }
