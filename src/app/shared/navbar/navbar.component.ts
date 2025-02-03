ed0a0a55ade14ef7cc3e0bad4023b05d79afd7c1
Jonas Gersting <jonasgersting@web.de>
2025-02-03 14:56:08 +0100

    menu responsive

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -9,17 +9,34 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
-  selectedLang: string = 'DE'; 
+  selectedLang: string = 'DE';
   selectedLink: string = '';
-  
-
+  menuOpen: boolean = false;
 
   setLanguage(lang: string): void {
     this.selectedLang = lang;
   }
 
-  setLink(link: string){
+  setLink(link: string): void {
     this.selectedLink = link;
+    this.closeMenu(); // Menü schließen
   }
 
+  toggleRespMenu(): void {
+    this.menuOpen = !this.menuOpen;
+    this.updateBodyScroll();
+  }
+
+  closeMenu(): void {
+    this.menuOpen = false;
+    this.updateBodyScroll();
+  }
+
+  private updateBodyScroll(): void {
+    if (this.menuOpen) {
+      document.body.classList.add('no-scroll');
+    } else {
+      document.body.classList.remove('no-scroll');
+    }
+  }
 }
